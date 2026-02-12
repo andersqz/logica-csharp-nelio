559369b85d4488fafedeef4a1572e9a04da4d0fe
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-12 17:44:30 -0300

    exercicio 1008 resolvido

---
+++ beecrowd/ex1008/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        int number = Int32.Parse(Console.ReadLine()!);
+        int hours = Int32.Parse(Console.ReadLine()!);
+        double hourlyWage = Double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+        double salary = hourlyWage * hours;
+        System.Console.WriteLine($"Number = {number}");
+        System.Console.WriteLine($"Salary = {salary.ToString("F2", CultureInfo.InvariantCulture)}");
+
+
+    }
+}
