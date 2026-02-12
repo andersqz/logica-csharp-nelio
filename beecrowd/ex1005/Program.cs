614676c0d42dc6711e666becad8f6f932b44fadb
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-12 13:29:57 -0300

    exercicio 1005 resolvido

---
+++ beecrowd/ex1005/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        double a = Double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        double b = Double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+        double media = ((a * 3.5) + (b * 7.5)) / (3.5 + 7.5);
+
+        System.Console.WriteLine($"MEDIA = {media.ToString("F5", CultureInfo.InvariantCulture)}");
+    }
+}
