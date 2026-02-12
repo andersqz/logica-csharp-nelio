2422ef2604e2f26111326254b0962104cabfe4a2
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-12 13:03:03 -0300

    exercicio 1004 resolvido

---
+++ beecrowd/ex1004/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int a = Int32.Parse(Console.ReadLine()!);
+        int b = Int32.Parse(Console.ReadLine()!);
+
+        int prod = a * b;
+
+        System.Console.WriteLine($"PROD = {prod}");
+    }
+}
