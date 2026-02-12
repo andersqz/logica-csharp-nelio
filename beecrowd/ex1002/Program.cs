d57a7b72138996845ebe9207b522ebd74a3f18e5
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-12 12:52:33 -0300

    exercicio 1002 resolvido

---
+++ beecrowd/ex1002/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        double area;
+        double raio;
+        const double PI = 3.14159;
+
+        raio = Double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+        area = PI * (Math.Pow(raio, 2));
+
+        System.Console.WriteLine($"A={area.ToString("F4", CultureInfo.InvariantCulture)}");
+
+        
+    }
+}
