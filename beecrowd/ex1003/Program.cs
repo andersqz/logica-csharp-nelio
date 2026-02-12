9b358b54d5c9df6a319868df18661ed1f33ce3dd
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-12 12:58:05 -0300

    exercicio 1003 resolvido

---
+++ beecrowd/ex1003/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = Int32.Parse(Console.ReadLine()!);
+        int b = Int32.Parse(Console.ReadLine()!);
+
+        int soma = a + b;
+
+        System.Console.WriteLine($"SOMA = {soma}");
+    }
+}
