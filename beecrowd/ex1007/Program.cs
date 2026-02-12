2915558b93c266a9caddc8055888912b0fc991e3
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-12 16:16:17 -0300

    exercicio 1007 realizado

---
+++ beecrowd/ex1007/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = Int32.Parse(Console.ReadLine()!);
+        int b = Int32.Parse(Console.ReadLine()!);
+        int c = Int32.Parse(Console.ReadLine()!);
+        int d = Int32.Parse(Console.ReadLine()!);
+
+        int diferenca = (a * b) - (c * d);
+
+        System.Console.WriteLine($"DIFERENCA = {diferenca}");
+    }
+}
