8021d21262eaf4f093c997c6b6e77b2016c00477
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-12 14:57:21 -0300

    exercicio 1006 resolvido

---
+++ beecrowd/ex1006/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        double a = Double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        double b = Double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        double c = Double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+        double media = ((a * 2) + (b * 3) + (c * 5)) / (2 + 3 + 5);
+
+        System.Console.WriteLine($"MEDIA = {media.ToString("F1", CultureInfo.InvariantCulture)}");
+    }
+}
