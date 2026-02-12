30c73d61686ca140fa3d4d8e53f48a95a49ec51b
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-12 10:13:57 -0300

    exercicio 1001 resolvido

---
+++ beecrowd/ex1001/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = Int32.Parse(Console.ReadLine()!);
+        int b = Int32.Parse(Console.ReadLine()!);
+
+        int x = a + b;
+
+        System.Console.WriteLine($"X = {x}");
+
+    }
+}
