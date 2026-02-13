1219e86fd1ca722d2378d7f71932e1092b4912f9
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-13 03:04:27 -0300

    exercicio 1014 resolvido

---
+++ beecrowd/ex1014/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        int distancia = Int32.Parse(Console.ReadLine()!);
+        double gas = Double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+        double consumo = distancia / gas;
+
+        System.Console.WriteLine($"{consumo.ToString("F3", CultureInfo.InvariantCulture)} km/l");
+
+
+    }
+}
