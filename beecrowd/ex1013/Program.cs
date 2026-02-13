02ffdfa36a319ecf9095a3ae5ffe3c6fa4e2f052
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-13 01:38:06 -0300

    exercicio 1013 resolvido

---
+++ beecrowd/ex1013/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] valores = Console.ReadLine()!.Split(' ');
+
+        int a = Int32.Parse(valores[0]);
+        int b = Int32.Parse(valores[1]);
+        int c = Int32.Parse(valores[2]);
+
+        int maiorAB = (a + b + (Math.Abs(a - b))) / 2;
+        int maiorABC = (maiorAB + c + Math.Abs(maiorAB - c)) / 2;
+
+        System.Console.WriteLine(maiorABC + " eh o maior");
+    }   
+}
