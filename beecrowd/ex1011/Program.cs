fcf0b85b880312f0243a7f37b9f50333680a43cf
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-13 01:11:13 -0300

    exercicio 1011 resolvido

---
+++ beecrowd/ex1011/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        double raio;
+        double volume;
+        const double PI = 3.14159;
+
+        raio = Double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        volume = (4 / 3.0) * PI * (Math.Pow(raio, 3));
+
+        System.Console.WriteLine($"VOLUME = {volume.ToString("F3", CultureInfo.InvariantCulture)}");
+    }
+}
