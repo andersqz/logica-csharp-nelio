06bc8960d8f081a8dabb07a1c879325f3f6475d6
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-13 00:47:46 -0300

    exercicio 1009 resolvido

---
+++ beecrowd/ex1009/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        string nome = Console.ReadLine() ?? "";
+        double salarioFixo = Double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        double totalVendas = Double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+        double totalAreceber = salarioFixo + (totalVendas * 0.15);
+
+        System.Console.WriteLine($"TOTAL = {totalAreceber.ToString("F2", CultureInfo.InvariantCulture)}");
+
+    }
+}
