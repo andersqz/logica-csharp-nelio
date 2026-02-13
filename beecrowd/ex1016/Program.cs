8371eba2b662c209a975214d208a9a472a855342
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-13 03:25:46 -0300

    exercicio 1016 resolvido

---
+++ beecrowd/ex1016/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int km = Int32.Parse(Console.ReadLine()!);
+
+        int minutos = km * 2;
+
+        System.Console.WriteLine($"{minutos} minutos");
+    }
+}
