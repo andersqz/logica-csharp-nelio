15f8e58cdac8b27792dd82e57bb49b3bcbedae08
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-13 13:14:35 -0300

    exercicio 1019 resolvido

---
+++ beecrowd/ex1019/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, 
+e informe-o expresso no formato horas:minutos:segundos.
+
+Entrada
+O arquivo de entrada contém um valor inteiro N.
+
+Saída
+Imprima o tempo lido no arquivo de entrada (segundos), convertido para horas:minutos:segundos, conforme exemplo fornecido.*/
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = Int32.Parse(Console.ReadLine()!);
+
+        int horas = n / 3600;
+        int resto = n % 3600;
+        int minutos = resto / 60;
+        int segundos = resto % 60;
+
+        System.Console.WriteLine($"{horas}:{minutos}:{segundos}"); 
+
+    }
+}
