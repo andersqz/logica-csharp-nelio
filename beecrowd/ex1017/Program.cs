92d1ca0d438af2c23f902bbc391782eb82365eea
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-13 11:06:47 -0300

    exercicio 1017 resolvido

---
+++ beecrowd/ex1017/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        int tempoGasto = Int32.Parse(Console.ReadLine()!);
+        int velocidadeMedia = Int32.Parse(Console.ReadLine()!);
+
+        int distancia = tempoGasto * velocidadeMedia;
+        double litros = distancia / 12.0;
+
+        System.Console.WriteLine(litros.ToString("F3", CultureInfo.InvariantCulture));
+
+    }
+}
