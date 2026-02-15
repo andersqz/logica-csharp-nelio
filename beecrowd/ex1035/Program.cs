1bc09233c212232513360a93b539d33bb239d13f
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-15 19:53:09 -0300

    exercicio 1035 resolvido

---
+++ beecrowd/ex1035/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Win32.SafeHandles;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b, c, d;
+
+        string[] vet = Console.ReadLine()!.Split(' ');
+        a = Int32.Parse(vet[0]);
+        b = Int32.Parse(vet[1]);
+        c = Int32.Parse(vet[2]);
+        d = Int32.Parse(vet[3]);
+
+        if (b > c && d > a && (c + d) > (a + b) && (c > 0 && d > 0) && (a % 2 == 0))
+        {
+            System.Console.WriteLine("Valores aceitos");
+        }
+        else
+        {
+            System.Console.WriteLine("Valores nao aceitos");
+        }
+
+        Console.ReadLine();
+        
+    }
+}
