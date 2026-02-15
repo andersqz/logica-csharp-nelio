1d6423779288df736bc5258cfa19531b3ac78c4d
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-15 19:45:40 -0300

    exercicio 1113 resolvido

---
+++ beecrowd/ex1113/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x, y;
+
+        string[] vet = Console.ReadLine()!.Split(' ');
+        x = Int32.Parse(vet[0]);
+        y = Int32.Parse(vet[1]);
+        
+        while (x != y)
+        {
+            if (x < y)
+                System.Console.WriteLine("Crescente");
+            else
+            System.Console.WriteLine("Decrescente");
+
+            vet = Console.ReadLine()!.Split(' ');
+            x = Int32.Parse(vet[0]);
+            y = Int32.Parse(vet[1]);
+
+        }
+    }
+}
