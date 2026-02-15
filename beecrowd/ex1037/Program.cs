81690a356d46c6476cc1e7d5a7ce0934bb0fb8ef
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-15 20:03:12 -0300

    exercicio 1037 resolvido

---
+++ beecrowd/ex1037/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        double number;
+
+        number = Double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+        if (number >= 0 && number <= 25)
+            System.Console.WriteLine("Intervalo [0,25]");
+        else if (number > 25 && number <= 50)
+            System.Console.WriteLine("Intervalo [25,50]");
+        else if (number > 50 && number <= 75)
+            System.Console.WriteLine($"Intervalo [50,75]");
+        else if (number > 75 && number <= 100)
+            System.Console.WriteLine("Intervalo [75,100]");
+    }   
+}
