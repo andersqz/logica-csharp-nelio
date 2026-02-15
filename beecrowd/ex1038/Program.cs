b9ce2a93b9f44eb4bce8fcb94e4dcf203794c8ec
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-15 20:13:14 -0300

    exercicio 1038 resolvido

---
+++ beecrowd/ex1038/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        int x, y; 
+        double preco;
+        string[] vet = Console.ReadLine()!.Split(' ');
+
+        x = Int32.Parse(vet[0]);
+        y = Int32.Parse(vet[1]);
+
+        switch (x)
+        {
+            case 1:
+                preco = y * 4.00;
+                System.Console.WriteLine($"Total: R$ {preco.ToString("F2", CultureInfo.InvariantCulture)}");
+                break;
+            case 2:
+                preco = y * 4.50;
+                System.Console.WriteLine($"Total: R$ {preco.ToString("F2", CultureInfo.InvariantCulture)}");
+                break;
+            case 3:
+                preco = y * 5.00;
+                System.Console.WriteLine($"Total: R$ {preco.ToString("F2", CultureInfo.InvariantCulture)}");
+                break;
+            case 4:
+                preco = y * 2.00;
+                System.Console.WriteLine($"Total: R$ {preco.ToString("F2", CultureInfo.InvariantCulture)}");
+                break;
+            case 5:
+                preco = y * 1.50;
+                System.Console.WriteLine($"Total: R$ {preco.ToString("F2", CultureInfo.InvariantCulture)}");
+                break;
+            default:
+                System.Console.WriteLine("VALOR INVALIDO");
+                break;
+        }
+    }
+}
