3336aa763408bd52fdf00935382775db6b811300
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-16 12:43:37 -0300

    exercicio 1071 resolvido

---
+++ beecrowd/ex1071/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+
+        int x, y, min, max; 
+        int soma = 0;
+
+        x = int.Parse(Console.ReadLine()!);
+        y = int.Parse(Console.ReadLine()!);
+
+        if (x < y) {
+            min = x;
+            max = y;
+        } else {
+            min = y;
+            max = x;
+        }
+
+        for ( int i = min + 1; i < max; i++ ) {
+            if (i % 2 != 0) {
+                soma = soma + i;
+            }
+        }
+
+        System.Console.WriteLine(soma);
+    }
+    
+}
