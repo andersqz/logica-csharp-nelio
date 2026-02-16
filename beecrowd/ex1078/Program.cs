b16eda7daee5d4d8d382b78ee417730c056e74c8
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-02-16 09:49:26 -0300

    exercicio 1078 resolvido

---
+++ beecrowd/ex1078/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n;
+
+        n = Int32.Parse(Console.ReadLine()!);
+
+        for ( int i = 1; i <= 10; i++ ) {
+            Console.WriteLine($"{i} X {n} = {i * n}");
+        }
+    }
+}
