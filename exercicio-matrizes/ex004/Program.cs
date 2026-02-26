0bd0922a55c36a683d284715777b59c9851ac7cf
andersqz <anderson.carvalho@ufn.edu.br>
2026-02-26 13:17:37 -0300

    exercicio de matriz 004 resolvido

--- exercicio-matrizes/ex004/Program.cs
+++ exercicio-matrizes/ex004/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine()!);
+        int[,] matriz = new int[n, n];
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            for (int j = 0; j < n; j++)
+            {
+                matriz[i, j] = int.Parse(s[j]);
+            }
+        }
+
+
+        int acumulador = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int soma = 0;
+            for (int j = 0; j < n; j++)
+            {
+                    if (j > i)
+                {
+                    acumulador += matriz[i, j];
+                }
+            }
+            System.Console.WriteLine(acumulador);
+        }
+    }
+}
